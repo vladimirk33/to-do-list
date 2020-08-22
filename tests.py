8c76c7009db64623703f885faba52e7aeba4d0a0
Vladimir <mr.katenin@mail.ru>
2020-08-22 16:08:11 +0300

    added delete mode and func decomp

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
 from hstest.check_result import CheckResult
 from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
-
 from datetime import datetime, timedelta
 from typing import List
-import shutil
 import os
+import shutil
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,7 +15,9 @@ menu = """
 1) Today's tasks
 2) Week's tasks
 3) All tasks
-4) Add task
+4) Missed tasks
+5) Add task
+6) Delete task
 0) Exit
 """.strip().lower()
 
@@ -34,6 +34,7 @@ weekdays = [
 
 class ToDoList(StageTest):
     db_name = 'todo.db'
+    tasks_before_delete = None
     is_completed = False
 
     def generate(self) -> List[TestCase]:
@@ -62,6 +63,17 @@ class ToDoList(StageTest):
                 stdin=[self.add_tasks_with_deadlines,
                        '3',
                        self.check_all_tasks_list]
+            ),
+            TestCase(
+                stdin=[self.check_missed_tasks,
+                       self.check_missed_tasks_ignore_output,
+                       self.check_list_of_missed_tasks]
+            ),
+            TestCase(
+                stdin=[
+                    self.check_delete_task,
+                    self.delete_tasks,
+                    self.check_if_tasks_deleted]
             )
         ]
 
@@ -70,8 +82,8 @@ class ToDoList(StageTest):
         if menu in reply.lower():
             return CheckResult.correct()
         else:
-            return CheckResult.wrong('Your program doesn\'t show the menu from example\n'
-                                     '  Make sure you didn\'t print any extra spaces')
+            return CheckResult.wrong('Your program doesn\'t show the menu from example\n '
+                                     ' Make sure you didn\'t print any extra spaces')
 
     def check_db_file(self, reply, attach):
         if not os.path.exists('todo.db'):
@@ -111,7 +123,7 @@ class ToDoList(StageTest):
                         day.title()))
 
         today = datetime.today().date()
-        return '4\nFirst task\n{}\n4\nSecond task\n{}\n1'.format(today, today)
+        return '5\nFirst task\n{}\n5\nSecond task\n{}\n1'.format(today, today)
 
     def check_added_task(self, output):
         tasks = self.execute('SELECT * FROM task')
@@ -144,8 +156,8 @@ class ToDoList(StageTest):
         first_date = datetime.today().date()
         second_date = first_date + timedelta(days=3)
         last_date = first_date + timedelta(days=6)
-        test_input = "4\nDeadline in 3 days\n{}\n4\nDeadline in 6 days\n{}\n4\nDeadline is today\n{}" \
-            .format(second_date, last_date, first_date).strip()
+        test_input = "5\nDeadline is today\n{}\n5\nDeadline in 3 days\n{}\n5\nDeadline in 6 days\n{}" \
+            .format(first_date, second_date, last_date).strip()
         return test_input
 
     def ignore_output(self, output):
@@ -192,8 +204,7 @@ class ToDoList(StageTest):
 
         if 'deadline in 3 days' not in second_block:
             return CheckResult.wrong(
-                'When you output the week\'s tasks the fourth date doesn\'t contain added task for which deadline is'
-                ' in 4 days.')
+                'When you output the week\'s tasks the fourth date doesn\'t contain added task for which deadline is in 4 days.')
 
         if (last_date_month not in last_block
                 or str(last_date_day) not in last_block
@@ -204,8 +215,7 @@ class ToDoList(StageTest):
 
         if 'deadline in 6 days' not in last_block:
             return CheckResult.wrong(
-                'When you output the week\'s tasks the last date doesn\'t contain added task for which deadline is'
-                ' in 6 days.')
+                'When you output the week\'s tasks the last date doesn\'t contain added task for which deadline is in 6 days.')
 
         self.is_completed = True
         return '0'
@@ -234,6 +244,65 @@ class ToDoList(StageTest):
         if first > second or second > third:
             return CheckResult.wrong("In the all tasks list tasks should be sorted by their deadlines!")
 
+        self.is_completed = True
+        return '0'
+
+    def check_missed_tasks(self, output):
+        today = datetime.today().date()
+        minus_one_day = today - timedelta(days=1)
+        minus_two_days = today - timedelta(days=2)
+        return '5\nFirst missed task\n{}\n5\nSecond missed task\n{}'.format(minus_two_days, minus_one_day)
+
+    def check_missed_tasks_ignore_output(self, output):
+        return '4'
+
+    def check_list_of_missed_tasks(self, output):
+        if 'missed tasks' not in output.lower():
+            return CheckResult.wrong('Your program doesn\'t show missed tasks!')
+
+        blocks = output.strip().split('\n\n')
+        if len(blocks) != 2:
+            return CheckResult.wrong(
+                'There is something wrong with format of output. Please make sure that you print only one empty line after printing missed tasks!')
+
+        tasks = blocks[0].lower()
+
+        if ('first missed task' not in tasks
+                or 'second missed task' not in tasks):
+            return CheckResult.wrong('When you print missed task you don\'t print all of them!')
+
+        lines = tasks.splitlines()
+        index_of_first_task = 0
+        index_of_second_task = 0
+        for i, line in enumerate(lines):
+            if 'first missed task' in line:
+                index_of_first_task = i
+            if 'second missed task' in line:
+                index_of_second_task = i
+
+        if index_of_first_task > index_of_second_task:
+            return CheckResult.wrong('Missed tasks should be sorted by their deadlines!')
+
+        self.is_completed = True
+        return '0'
+
+    def check_delete_task(self, output):
+        self.execute('DELETE FROM task')
+        first_date = datetime.today().date()
+        second_date = first_date + timedelta(days=3)
+        last_date = first_date + timedelta(days=6)
+        test_input = "5\nDeadline is today\n{}\n5\nDeadline in 3 days\n{}\n5\nDeadline in 6 days\n{}\n6" \
+            .format(first_date, second_date, last_date).strip()
+        return test_input
+
+    def delete_tasks(self, output):
+        ToDoList.tasks_before_delete = len(self.execute('SELECT * FROM task'))
+        return '1\n6\n1\n6\n1'
+
+    def check_if_tasks_deleted(self, output):
+        tasks_after_delete = len(self.execute('SELECT * FROM task'))
+        if not tasks_after_delete < ToDoList.tasks_before_delete:
+            return CheckResult.wrong('Once a task has been deleted, there should be less rows in the table.')
         self.is_completed = True
         return '0'
 
--- todolist/todolist.py
+++ todolist/todolist.py
@@ -5,26 +5,38 @@ from sqlalchemy.orm import sessionmaker
 
 from datetime import datetime, timedelta
 
+# create todo.db file
 engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 Base = declarative_base()
 
 
 class Table(Base):
+    """Create DB Table."""
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
     task = Column(String)
     deadline = Column(Date, default=datetime.today())
 
     def __repr__(self):
+        """Print task column."""
         return self.task
 
 
+# create DB session
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
+def print_tasks(rows):
+    """Print tasks in {i. Task. Day Month} format."""
+    for i in range(len(rows)):
+        print(f"{i + 1}. {rows[i].task}. {rows[i].deadline.day} "
+              + f"{rows[i].deadline.strftime('%b')}")
+
+
 def today_tasks():
+    """Print today's tasks."""
     today = datetime.today()
     rows = session.query(Table).filter(Table.deadline == today.date()).all()
     print(f"\nToday {today.day} {today.strftime('%b')}:")
@@ -37,12 +49,14 @@ def today_tasks():
 
 
 def week_tasks():
+    """Print week's tasks."""
+    # weekdays tuple for .weekday method
     WEEKDAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
                 "Saturday", "Sunday")
     today = datetime.today()
     print()
     for day_number in range(7):
-        day = today + timedelta(days=day_number)
+        day: datetime = today + timedelta(days=day_number)
         print(f"{WEEKDAYS[day.weekday()]} {day.day} {day.strftime('%b')}:")
         rows = session.query(Table).filter(Table.deadline == day.date()).all()
         if not len(rows):
@@ -53,20 +67,31 @@ def week_tasks():
             print()
 
 
-
 def all_tasks():
+    """Print all tasks."""
     rows = session.query(Table).order_by(Table.deadline).all()
     print("\nAll tasks:")
     if not len(rows):
         print("Nothing to do!\n")
     else:
-        for i in range(len(rows)):
-            print(f"{i + 1}. {rows[i].task}. {rows[i].deadline.day} "
-                  + f"{rows[i].deadline.strftime('%b')}")
+        print_tasks(rows)
+        print()
+
+
+def missed_tasks():
+    """Print missed tasks."""
+    today = datetime.today()
+    rows = session.query(Table).filter(Table.deadline < today.date()).all()
+    print("\nMissed tasks:")
+    if not len(rows):
+        print("Nothing is missed!\n")
+    else:
+        print_tasks(rows)
         print()
 
 
 def _get_correct_row(task):
+    """Return correct row for add_task function."""
     while True:
         try:
             deadline = [int(s) for s in input("Enter deadline\n").split("-")]
@@ -79,6 +104,7 @@ def _get_correct_row(task):
 
 
 def add_task():
+    """Add task to DB."""
     task = input("\nEnter task\n")
     new_row = _get_correct_row(task)
     session.add(new_row)
@@ -86,11 +112,36 @@ def add_task():
     print("The task has been added!\n")
 
 
+def delete_task():
+    """Delete task from DB."""
+    rows = session.query(Table).order_by(Table.deadline).all()
+    print("\nChoose the number of the task you want to delete:")
+    if not len(rows):
+        print("Nothing to delete!\n")
+    else:
+        print_tasks(rows)
+        while True:
+            try:
+                delete_number = int(input())
+                if delete_number:
+                    session.delete(rows[delete_number - 1])
+                    break
+                else:
+                    print("Print correct number")
+            except (ValueError, IndexError):
+                print("Print correct number")
+        session.commit()
+        print("The task has been deleted!\n")
+
+
 def menu():
+    """Menu with modes."""
+    menu_list = ["1) Today's tasks", "2) Week's tasks", "3) All tasks",
+                 "4) Missed tasks", "5) Add task", "6) Delete task", "0) Exit"]
     response = None
     while response != 0:
-        print("1) Today's tasks\n2) Week's tasks\n3) All tasks")
-        print("4) Add task\n0) Exit")
+        for menu_item in menu_list:
+            print(menu_item)
         try:
             response = int(input())
         except ValueError:
@@ -102,12 +153,17 @@ def menu():
         elif response == 3:
             all_tasks()
         elif response == 4:
+            missed_tasks()
+        elif response == 5:
             add_task()
+        elif response == 6:
+            delete_task()
         elif response == 0:
             print("\nBye!")
 
 
 def main():
+    """Starter."""
     menu()
 
 
